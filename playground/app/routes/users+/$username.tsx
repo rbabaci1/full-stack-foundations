844de8610d61715b459431dd1269f9fbce565f3f
rabah babaci <4rabah@gmail.com>
2024-02-18 15:00:27 -0800

    load the data in the username route

--- playground/app/routes/users+/$username.tsx
+++ playground/app/routes/users+/$username.tsx
@@ -1,10 +1,25 @@
-import { Link, useParams } from '@remix-run/react'
+import { json, DataFunctionArgs } from '@remix-run/node'
+import { Link, useLoaderData, useParams } from '@remix-run/react'
+import { db } from '#app/utils/db.server.ts'
+
+export const loader = async ({ params }: DataFunctionArgs) => {
+	const { username } = params
+
+	const user = db.user.findFirst({
+		where: {
+			username: { equals: username },
+		},
+	})
+
+	return json({ user: { name: user.name, username: user.username } })
+}
 
 export default function ProfileRoute() {
-	const params = useParams()
+	const data = useLoaderData<typeof loader>()
+
 	return (
-		<div className="container mb-48 mt-36 border-4 border-green-500">
-			<h1 className="text-h1">{params.username}</h1>
+		<div className="container mb-48 mt-36">
+			<h1 className="text-h1">{data.user.name ?? data.user?.username}</h1>
 			<Link to="notes" className="underline">
 				Notes
 			</Link>
